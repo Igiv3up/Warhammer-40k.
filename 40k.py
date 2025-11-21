58338d8bc79ef491a874fb8af4dccb391c96cc69
Wyatt Deane <wyattocta@gmail.comm>
2025-11-21 10:20:40 -0500

    First push

---
+++ 40k.py
@@ -0,0 +1,13 @@
+import random
+names1 = ['Black Templars, Dark Angels, Angels of Vengence, Blood Angels, Lamenters']
+print('black Templars Secound founding of the Imperial fist their Pimarch is Rogal Dorn')
+print('The Black Templars are known for their anger not towards their enemies but everyone')
+print('They see the Emepror of mankind as a god of some sort')
+print('The Black Templars have a speical soldier called the Emperors Chapmion he can only become a Champion')
+print('If he sees the Emepror in a vision, their first Emperors Champion was Sigismund')
+print('Sigismund actually met both Rogal Dorn and the Emepror of mankind himself')
+print('in fact he was actually the first captain of the Imperial fist')
+print('Sigismund was so powerful that at 1000 years old Abaddon struggled to kill Sigismund')
+print('Sigismund is consider to be one of if not the strongest space marine in the gaxaly with the his sword')
+print('His sword is called the Black Sword made and given by The Emperor of mankind himself ')
+print('Black Templars almost never fall to Chaos only one known case is Castellan Draco')
