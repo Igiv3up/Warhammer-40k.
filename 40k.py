b36e2ac2c5602746bd99995ae162083fb889d72e
Wyatt Deane <wyattocta@gmail.comm>
2025-11-21 10:23:47 -0500

    Added some new info about a possbile hereticd

--- 40k.py
+++ 40k.py
@@ -11,3 +11,4 @@ print('Sigismund was so powerful that at 1000 years old Abaddon struggled to kil
 print('Sigismund is consider to be one of if not the strongest space marine in the gaxaly with the his sword')
 print('His sword is called the Black Sword made and given by The Emperor of mankind himself ')
 print('Black Templars almost never fall to Chaos only one known case is Castellan Draco')
+print('But even then we have no idea what he has done even as they never tell us')
