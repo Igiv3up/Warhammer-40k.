d375ad9d57bfa54ba1b619c053481bb1f40a70d9
Wyatt Deane <wyattocta@gmail.comm>
2026-01-14 08:52:43 -0500

    Describe what you changed

--- 40k.py
+++ 40k.py
@@ -1,14 +1,28 @@
-import random
-names1 = ['Black Templars, Dark Angels, Angels of Vengence, Blood Angels, Lamenters']
-print('black Templars Secound founding of the Imperial fist their Pimarch is Rogal Dorn')
-print('The Black Templars are known for their anger not towards their enemies but everyone')
-print('They see the Emepror of mankind as a god of some sort')
-print('The Black Templars have a speical soldier called the Emperors Chapmion he can only become a Champion')
-print('If he sees the Emepror in a vision, their first Emperors Champion was Sigismund')
-print('Sigismund actually met both Rogal Dorn and the Emepror of mankind himself')
-print('in fact he was actually the first captain of the Imperial fist')
-print('Sigismund was so powerful that at 1000 years old Abaddon struggled to kill Sigismund')
-print('Sigismund is consider to be one of if not the strongest space marine in the gaxaly with the his sword')
-print('His sword is called the Black Sword made and given by The Emperor of mankind himself ')
-print('Black Templars almost never fall to Chaos only one known case is Castellan Draco')
-print('But even then we have no idea what he has done even as they never tell us')
+while True:
+    print("Space marine chapter profiles")
+    print("1. Imperial Fists")
+    print("2. Black Templars")
+    print("3. Exit")
+
+    choice = input("Pick (1-3): ")
+
+    if choice == "1":
+        print("Imperial Fists info:")
+        print("Primarch: Rogal Dorn")
+        print("Codex: They follow it")
+        print("Colors: Yellow")
+        print("Known for: Fortifications")
+
+    elif choice == "2":
+        print("Black Templars info:")
+        print("1Primarch: From Rogal Dorn")
+        print("Codex: They don't follow it")
+        print("Colors: Black/White")
+        print("Known for: Crusades")
+
+    elif choice == "3":
+        print("For the Emperor")
+        break
+
+    else:
+        print("Just pick 1, 2, or 3")
---
+++ readmefile.py
@@ -0,0 +1,127 @@
+# Each of you will be doing one loyalist Space Marines chapters. I have compiled a list of all the loyalist chapters you can choose from.
+#
+# (doc notes blank means hasn’t been done yet, X means someone has done it)
+#
+#
+#
+# List of all loyalist Space Marine chapters
+#
+#
+# Dark angels:
+# Angel of Aboulstion
+# Angels of Redemption
+# Angels of Vengeance
+# Consecrators
+# Disciples of Caliban
+# Guardian of the Covenant
+# Angels of Vigilance
+# Angels of Defiance
+# Blade of Vegence
+# White scars:
+#  Matines warriors
+# Solar Hawks
+# Storm lords
+# Marines Errant
+# Dark hunters
+# Desotryers
+# Space wolves:
+# Wolf brothers
+# Wolfspears
+# X Imperial fists:
+# X Black Templars
+# Crimson Fists
+# Excoriators
+# Fist Exmplars
+# Soul Drinkers
+# Celestial lions
+# Execuntioners
+# Subjugators
+# Sons of Phoeinx
+# Hammers of Dorn
+# Shadow Wolves
+# Iron knights
+# Blood angels:
+# Blood Drinkers
+# Flesh Terrers
+# Angels Encarmine
+# Angels Sanguine
+# Angels of Vermilion
+# Carimine Blades
+# Lamnetrs
+# Knights of the Blood
+# Sanguines Spears
+# Charnel Guard
+# Flesh Eaters
+# Blood Swords
+# Iron hands:
+# Brazen Claws
+# Sons of Medusa
+# Red Talons
+# Iron Lords
+# Knights of the Iron Cog
+# Ultramarines:
+# Aurora
+# Black Consuls
+# Doom Eagles
+# Gensies
+# Howling Griffions
+# Libators
+# Novamarines
+# Patriarchs of Ullixe
+# Praetors of Orpheus
+# White consuls
+# Red Consuls
+# Sycths of the Emperor
+# Morficators
+# Fire Angels
+# Star Phatomns
+# Nemesis chapters
+# Angels of fury
+# Invaders
+# Knights of Ultramar
+# Storm Giants
+# Exorcist
+# Emperor’s Spears
+# Iron Snakes
+# Void Tridents
+# Novamarines
+# Fulmainators
+# Salamanders:
+# Black Dragons
+# Dark Karkens
+# Silver Drakes
+# Raven guard:
+# Black Guards
+# Revilers
+# Raptors
+# Knights of Raven
+# Carcharodons Astra
+# Unknown foundings:
+# Minotaurs
+# Death spectras
+# Sons of Antaeus
+# Blood ravens
+# Storm Wanders
+# Marines Malevolent
+# Fire Hawks
+# Legions of the dammed
+#
+#
+#
+#
+# When coding this you’re going to have to add a few things
+# Does the space marine chapter follow the codex of astartes
+# Who is their Primarch
+# Who are they the successor chapter of from the original loyalist legion
+# If they are an unknown founding chapter explain why they are a unknown founding
+# Tell me 3 important Figures of that Space marine chapter
+# What Planet is the Space marine chapter from
+# If the space marine chapter isn’t from a planet What is their birth place
+# What is the Space marine chapter they are best known for
+#
+#
+#
+#
+#
+#
+#
